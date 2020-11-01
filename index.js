4ec8d64a59c45b74a3d99b46ff67ea675af2c07d
sangwon <junothyme@gmail.com>
2020-11-01 18:50:56 +0900

    feat: introduced body-parser to the project

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+
+const { User } = require('./models/User');
+
 require('dotenv').config();
 
-const port = 5000;
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
-const mongoose = require('mongoose');
+const PORT = 5000;
+
 console.log(process.env.PASSWORD);
 mongoose
     .connect(
@@ -19,5 +26,16 @@ mongoose
     .then(() => console.log('mondodb connected'));
 
 app.get('/', (req, res) => res.send('Hello World'));
+app.post('/register', (req, res) => {
+    const user = new User(req.body);
+    user.save((err, doc) => {
+        if (err) {
+            return res.json({ success: false, err });
+        }
+        return res.status(200).json({
+            success: true,
+        });
+    });
+});
 
-app.listen(port, () => console.log(`Listening on ${port}`));
+app.listen(port, () => console.log(`Listening on ${PORT}`));
