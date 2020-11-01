5ff05b4c1942414b5164c0150f68ac147aba6138
sangwon <junothyme@gmail.com>
2020-11-01 18:42:00 +0900

    feat: connected mongodb cloud

--- index.js
+++ index.js
@@ -1,7 +1,23 @@
-const express = require("express");
+const express = require('express');
 const app = express();
+require('dotenv').config();
+
 const port = 5000;
 
-app.get("/", (req, res) => res.send("Hello World"));
+const mongoose = require('mongoose');
+console.log(process.env.PASSWORD);
+mongoose
+    .connect(
+        `mongodb+srv://won:${process.env.PASSWORD}@boilerplate.y0wgj.mongodb.net/test?retryWrites=true&w=majority`,
+        {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+            useCreateIndex: true,
+            useFindAndModify: true,
+        }
+    )
+    .then(() => console.log('mondodb connected'));
+
+app.get('/', (req, res) => res.send('Hello World'));
 
 app.listen(port, () => console.log(`Listening on ${port}`));
