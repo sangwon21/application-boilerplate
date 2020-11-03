ef74db76cf04570e65367e4d42425a8bb6092210
sangwon <junothyme@gmail.com>
2020-11-03 19:04:41 +0900

    feat: implemented auth middleware to check authorization

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const cookieParser = require('cookie-parser');
 
 const config = require('./config/key');
 const { User } = require('./models/User');
+const { auth } = require('./middleware/auth');
 
 require('dotenv').config();
 
@@ -65,6 +66,19 @@ app.post('/login', (req, res) => {
                 });
             });
         });
+    });
+});
+
+app.get('/api/users/auth', auth, (req, res) => {
+    res.status(200).json({
+        _id: req.user._id,
+        isAdmin: req.user.role === 0 ? false : true,
+        isAuth: true,
+        email: req.user.email,
+        name: req.user.name,
+        lastname: req.user.lastname,
+        role: req.user.role,
+        image: req.user.image,
     });
 });
 
--- models/User.js
+++ models/User.js
@@ -78,6 +78,19 @@ userSchema.methods.generateToken = function (cb) {
     });
 };
 
+userSchema.statics.findByToken = function (token, cb) {
+    const user = this;
+
+    jwt.verify(token, 'secretToken', function (err, decoded) {
+        user.findOne({ _id: decoded, token: token }, function (err, user) {
+            if (err) {
+                return cb(err);
+            }
+            cb(null, user);
+        });
+    });
+};
+
 const User = mongoose.model('User', userSchema);
 
 module.exports = {
